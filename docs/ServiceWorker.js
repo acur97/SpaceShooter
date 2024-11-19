7136c43c4c5c174e3b52fb5181f24e8a8fd5bf6f
Andres Camilo Urrego Restrepo <andres.urrego@polygon.us>
2024-11-18 21:34:21 -0500

    - remove again data caching (is really wrong implemented)

--- docs/ServiceWorker.js
+++ docs/ServiceWorker.js
@@ -1,33 +1,6 @@
-const cacheName = "PolygonUs-SpaceShooter-0.3.4";
-const contentToCache = [
-    "Build/7f46c4e10add95d60a7c6deacf727032.loader.js",
-    "Build/3fc20a04c3106e559c8ea8586bb34e1f.framework.js",
-    "Build/a35dccbff000c8dffcfdf33ca8da75be.data",
-    "Build/ed2ee966ea922d50754d163f9fe22253.wasm",
-    "TemplateData/style.css"
-
-];
 
 self.addEventListener('install', function (e) {
     console.log('[Service Worker] Install');
     
-    e.waitUntil((async function () {
-      const cache = await caches.open(cacheName);
-      console.log('[Service Worker] Caching all: app shell and content');
-      await cache.addAll(contentToCache);
-    })());
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith((async function () {
-      let response = await caches.match(e.request);
-      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
-      if (response) { return response; }
-
-      response = await fetch(e.request);
-      const cache = await caches.open(cacheName);
-      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
-      cache.put(e.request, response.clone());
-      return response;
-    })());
-});
