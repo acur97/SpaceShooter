25884955ed0b1945978ef8b8270a8e32fd02f16a
Andres Urrego <andres.urrego@polygon.us>
2025-04-22 15:16:51 -0500

    Hotfix solution: remove the specific item ui, use c# tooltip instead

--- Assets/_Scripts/Ships/Scriptables/Rounds/RoundScriptable.cs
+++ Assets/_Scripts/Ships/Scriptables/Rounds/RoundScriptable.cs
@@ -13,21 +13,18 @@ public class RoundScriptable : ScriptableObject
 [System.Serializable]
 public struct Group
 {
-    // Editor
-    public bool groupEnable;
-
-    public bool chained;
-    public float timeToStart;
-    public Enums.SpawnType spawnType;
-    [Range(-2, 2)] public float customFloat;
-    public float minTimeBetweenSpawn;
-    public float maxTimeBetweenSpawn;
+    [Tooltip("The \"Time to start\" starts counting together with the previous group")] public bool chained;
+    [Tooltip("Time to start this group when the previous one ends")] public float timeToStart;
+    [Tooltip("How the enemy appears")] public Enums.SpawnType spawnType;
+    [Tooltip("Float custom (Especific, etc)"), Range(-2, 2)] public float customFloat;
+    [Tooltip("Min time between each enemy's appearance")] public float minTimeBetweenSpawn;
+    [Tooltip("Max time between each enemy's appearance")] public float maxTimeBetweenSpawn;
 
     [Space]
-    public int count;
-    public ShipScriptable ship;
+    [Tooltip("Number of enemies")] public int count;
+    [Tooltip("Enemy")] public ShipScriptable ship;
 
     [Space]
-    public bool randomPowerUp;
-    public PowerUpBase spawnPowerUp;
+    [Tooltip("PowerUp to spawn at the end of this group (empty means none will spawn)")] public PowerUpBase spawnPowerUp;
+    [Tooltip("Use a random PowerUp from all the existing ones?")] public bool randomPowerUp;
 }
